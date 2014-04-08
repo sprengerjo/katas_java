600b68af39ab8037ac2618b0e66852bce44dce50
Jonas Sprenger <jonas.sprenger@gmx.de>
2014-04-08 23:25:39 +0200

    before state pattern refactoring

--- src/main/java/de/sprengerjo/game/TennisGame.java
+++ src/main/java/de/sprengerjo/game/TennisGame.java
@@ -9,7 +9,52 @@ package de.sprengerjo.game;
  */
 public class TennisGame {
 
+    private int playerOne = 0;
+    private int playerTwo = 0;
+
     String score(int i) {
-        return "love";
+        switch (i) {
+            case 0:
+                return "love";
+            case 1:
+                return "fifteen";
+            case 2:
+                return "thirty";
+            case 3:
+                return "fourty";
+        }
+        return null;
+    }
+
+    public String score() {
+        String score = "love all";
+        if (Math.abs(playerOne - playerTwo) > 1 && playerOne + playerTwo > 4) {
+            score = "winner " + obtainLeadingPayer();
+        } else if (playerOne != playerTwo && playerOne + playerTwo > 5) {
+            score = "advantage " + obtainLeadingPayer();
+        } else if (playerOne == playerTwo && playerOne >= 3) {
+            score = "deuce";
+        } else if (playerOne > 0 || playerTwo > 0) {
+            score = score(playerOne) + " " + score(playerTwo);
+        }
+        return score;
+    }
+
+    private String obtainLeadingPayer() {
+        String result = "player ";
+        if (playerOne > playerTwo) {
+            result += "one";
+        } else {
+            result += "two";
+        }
+        return result;
+    }
+
+    public void playerOneScores() {
+        playerOne++;
+    }
+
+    public void playerTwoScores() {
+        playerTwo++;
     }
 }
--- src/test/java/de/sprengerjo/game/TennisGameTest.java
+++ src/test/java/de/sprengerjo/game/TennisGameTest.java
@@ -1,6 +1,7 @@
 package de.sprengerjo.game;
 
 import org.hamcrest.core.IsEqual;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,13 +12,102 @@ import static org.hamcrest.core.IsEqual.equalTo;
  * User: cojo
  * Date: 06.04.14
  * Time: 15:12
- * To change this template use File | Settings | File Templates.
  */
 public class TennisGameTest {
+    TennisGame game = null;
+
+    @Before
+    public void setUp() {
+        game = new TennisGame();
+    }
 
     @Test
     public void score0MustBeLove() {
-        TennisGame game = new TennisGame();
         assertThat(game.score(0), equalTo("love"));
+    }
+
+    @Test
+    public void score1MustBeFifteen() {
+        assertThat(game.score(1), equalTo("fifteen"));
+    }
+
+    @Test
+    public void score2MustBeThirty() {
+        assertThat(game.score(2), equalTo("thirty"));
+    }
+
+    @Test
+    public void score3MustBeFourty() {
+        assertThat(game.score(3), equalTo("fourty"));
+    }
+
+    @Test
+    public void initalScoreMustBeLoveAll() {
+        assertThat(game.score(), equalTo("love all"));
+    }
+
+    @Test
+    public void playerOneScores() {
+        game.playerOneScores();
+        assertThat(game.score(), equalTo("fifteen love"));
+    }
+
+    @Test
+    public void playerTwoScores() {
+        game.playerTwoScores();
+        assertThat(game.score(), equalTo("love fifteen"));
+    }
+
+    @Test
+    public void deuce() {
+        scoreMany(4);
+        assertThat(game.score(), equalTo("deuce"));
+    }
+
+    @Test
+    public void advantangePlaywerOne() {
+        scoreMany(4);
+        game.playerOneScores();
+        assertThat(game.score(), equalTo("advantage player one"));
+    }
+
+    @Test
+    public void advantangePlaywerTwo() {
+        scoreMany(4);
+        game.playerTwoScores();
+        assertThat(game.score(), equalTo("advantage player two"));
+    }
+
+    @Test
+    public void winnerPlaywerOne() {
+        scoreMany(4);
+        game.playerOneScores();
+        game.playerOneScores();
+        assertThat(game.score(), equalTo("winner player one"));
+    }
+
+    @Test
+    public void winnerPlayerTwo() {
+        scoreMany(4);
+        game.playerTwoScores();
+        game.playerTwoScores();
+        assertThat(game.score(), equalTo("winner player two"));
+    }
+
+    @Test
+    public void sweepWinnerPlayerTwo() {
+        game.playerTwoScores();
+        game.playerTwoScores();
+        game.playerTwoScores();
+        game.playerTwoScores();
+        assertThat(game.score(), equalTo("winner player two"));
+    }
+
+    private void scoreMany(int points) {
+        int i = 0;
+        while (i++ < points) {
+            game.playerOneScores();
+            game.playerTwoScores();
+        }
     }
 }
